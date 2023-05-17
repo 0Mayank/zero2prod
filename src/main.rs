93e1d9862391fb25ad21ec35794b7db937618c7f
0Mayank <1mayanktomar@gmail.com>
2023-05-17 23:33:09 +0530

    connected to database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use lib::configuration::get_configuration;
+use lib::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
